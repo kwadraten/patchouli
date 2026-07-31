9334c307c2d2418fc3f206657ae1cb9d72d07516
squaresum <schild0van0man@duck.com>
2026-07-31 15:59:55 +0800

    chore: bump version to 0.2.6

--- scripts/package-windows.ps1
+++ scripts/package-windows.ps1
@@ -1,7 +1,7 @@
 param(
     [string]$Runtime = "win-x64",
     [string]$Configuration = "Release",
-    [string]$Version = "0.2.5"
+    [string]$Version = "0.2.6"
 )
 
 $ErrorActionPreference = "Stop"
